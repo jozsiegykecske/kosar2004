bd5bf7d0f0be7dfee52964dbd513b523d407d3df
jozsiegykecske <jozsiegykecske@gmail.com>
2021-09-28 08:56:09 +0200

    Add project files.

---
+++ kosar2004/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace kosar2004
+{
+  class Program
+  {
+    static List<kosar> lista = new List<kosar>();
+    static void Main(string[] args)
+    {
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      HetedikFeladat();
+      Console.ReadKey();
+    }
+
+    private static void HetedikFeladat()
+    {
+      Console.WriteLine("7.feladat");
+      Dictionary<string, int> dick = new Dictionary<string, int>();
+      foreach (var l in lista)
+      {
+        if (!dick.ContainsKey(l.Helyszin))
+        {
+          dick.Add(l.Helyszin,0);
+        }
+        else
+        {
+          dick[l.Helyszin]++;
+        }
+      }
+      foreach (var d in dick)
+      {
+        if (d.Value>20)
+        {
+          Console.WriteLine($"\t {d.Key}: {d.Value}");
+        }
+      }
+    }
+
+    private static void HatodikFeladat()
+    {
+      Console.WriteLine("6.feladat:");
+      foreach (var l in lista)
+      {
+        if (l.Idopont.ToShortDateString()=="2004. 11. 21.")
+        {
+          Console.WriteLine($"\t{l.Hazai}-{l.Idegen} ({l.Hazai_pont}:{l.Idegen_pont})");
+        }
+      }
+    }
+
+    private static void OtodikFeladat()
+    {
+      string csapat_neve ="";
+      foreach (var l in lista)
+      {
+        if (l.Hazai.Contains("Barcelona"))
+        {
+          csapat_neve = l.Hazai;
+        }
+      }
+      Console.WriteLine($"5. feladat: barcelonai csapat neve: {csapat_neve}");
+    }
+
+    private static void NegyedikFeladat()
+    {
+      int szamlalo = 0;
+      bool nincs = true;
+      while (szamlalo<lista.Count && nincs)
+      {
+        if (lista[szamlalo].Hazai_pont == lista[szamlalo].Idegen_pont)
+        {
+          nincs = false;
+        }
+        szamlalo++;
+      }
+      if (nincs)
+      {
+        Console.WriteLine("4.feladat: Volt döntetlen? Nem");
+      }
+      else
+      {
+        Console.WriteLine("4.feladat: Volt döntetlen? Igen");
+      }
+    }
+
+    private static void HarmadikFeladat()
+    {
+      int hazai = 0,idegen = 0;
+      foreach (var l in lista)
+      {
+        if (l.Hazai=="Real Madrid")
+        {
+          hazai++;
+        }
+        if (l.Idegen=="Real Madrid")
+        {
+          idegen++;
+        }
+      }
+      Console.WriteLine($"3. feladat: Real Madrid: Hazai: {hazai}, Idegen: {idegen}");
+    }
+
+    private static void MasodikFeladat()
+    {
+      StreamReader be = new StreamReader("eredmenyek.csv");
+      be.ReadLine();
+      while (!be.EndOfStream)
+      {
+        string[] valami = be.ReadLine().Split(';');
+        lista.Add(new kosar(valami[0],valami[1],Convert.ToInt32(valami[2]), Convert.ToInt32(valami[3]),valami[4],Convert.ToDateTime(valami[5])));
+      }
+      be.Close();
+    }
+  }
+}
---
+++ kosar2004/kosar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosar2004
+{
+  class kosar
+  {
+    private string hazai;
+
+    public string Hazai
+    {
+      get { return hazai; }
+      set { hazai = value; }
+    }
+    private string idegen;
+
+    public string Idegen
+    {
+      get { return idegen; }
+      set { idegen = value; }
+    }
+    private int hazai_pont;
+
+    public int Hazai_pont
+    {
+      get { return hazai_pont; }
+      set { hazai_pont = value; }
+    }
+    private int idegen_pont;
+
+    public int Idegen_pont
+    {
+      get { return idegen_pont; }
+      set { idegen_pont = value; }
+    }
+    private string helyszin;
+
+    public string Helyszin
+    {
+      get { return helyszin; }
+      set { helyszin = value; }
+    }
+    private DateTime idopont;
+
+    public DateTime Idopont
+    {
+      get { return idopont; }
+      set { idopont = value; }
+    }
+
+    public kosar(string hazai, string idegen, int hazai_pont, int idegen_pont, string helyszin, DateTime idopont)
+    {
+      this.hazai = hazai;
+      this.idegen = idegen;
+      this.hazai_pont = hazai_pont;
+      this.idegen_pont = idegen_pont;
+      this.helyszin = helyszin;
+      this.idopont = idopont;
+    }
+  }
+}
